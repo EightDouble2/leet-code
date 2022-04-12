595b4ad50add1c141af90ab41c918f7f95263242
JohnnyHao <johnny173862903@gmail.com>
2022-04-12 23:02:29 +0800

    2022-04-12

---
+++ [806]写字符串需要的行数.java
@@ -0,0 +1,68 @@
+import java.util.Arrays;
+
+/**
+<p>我们要把给定的字符串 <code>S</code>&nbsp;从左到右写到每一行上，每一行的最大宽度为100个单位，如果我们在写某个字母的时候会使这行超过了100 个单位，那么我们应该把这个字母写到下一行。我们给定了一个数组&nbsp;<code>widths</code>&nbsp;，这个数组&nbsp;widths[0] 代表 &#39;a&#39; 需要的单位，&nbsp;widths[1] 代表 &#39;b&#39; 需要的单位，...，&nbsp;widths[25] 代表 &#39;z&#39; 需要的单位。</p>
+
+<p>现在回答两个问题：至少多少行能放下<code>S</code>，以及最后一行使用的宽度是多少个单位？将你的答案作为长度为2的整数列表返回。</p>
+
+<pre>
+<strong>示例 1:</strong>
+<strong>输入:</strong> 
+widths = [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
+S = &quot;abcdefghijklmnopqrstuvwxyz&quot;
+<strong>输出:</strong> [3, 60]
+<strong>解释: 
+</strong>所有的字符拥有相同的占用单位10。所以书写所有的26个字母，
+我们需要2个整行和占用60个单位的一行。
+</pre>
+
+<pre>
+<strong>示例 2:</strong>
+<strong>输入:</strong> 
+widths = [4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
+S = &quot;bbbcccdddaaa&quot;
+<strong>输出:</strong> [2, 4]
+<strong>解释: 
+</strong>除去字母&#39;a&#39;所有的字符都是相同的单位10，并且字符串 &quot;bbbcccdddaa&quot; 将会覆盖 9 * 10 + 2 * 4 = 98 个单位.
+最后一个字母 &#39;a&#39; 将会被写到第二行，因为第一行只剩下2个单位了。
+所以，这个答案是2行，第二行有4个单位宽度。
+</pre>
+
+<p>&nbsp;</p>
+
+<p><strong>注:</strong></p>
+
+<ul>
+	<li>字符串&nbsp;<code>S</code> 的长度在&nbsp;[1, 1000] 的范围。</li>
+	<li><code>S</code> 只包含小写字母。</li>
+	<li><code>widths</code> 是长度为&nbsp;<code>26</code>的数组。</li>
+	<li><code>widths[i]</code>&nbsp;值的范围在&nbsp;<code>[2, 10]</code>。</li>
+</ul>
+<div><div>Related Topics</div><div><li>数组</li><li>字符串</li></div></div><br><div><li>👍 111</li><li>👎 0</li></div>
+*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+	public static final int MAX_WIDTH = 100;
+
+    public int[] numberOfLines(int[] widths, String s) {
+		int lines = 1;
+		int width = 0;
+		for (int i = 0; i < s.length(); i++) {
+			int need = widths[s.charAt(i) - 'a'];
+			width += need;
+			if (width > MAX_WIDTH) {
+				lines++;
+				width = need;
+			}
+		}
+		return new int[]{lines, width};
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+class Test {
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(new Solution().numberOfLines(new int[]{4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10}, "bbbcccdddaaa")));
+	}
+}
