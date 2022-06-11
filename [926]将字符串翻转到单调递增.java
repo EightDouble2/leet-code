de9692255ec73abedf6370982b4a656efa307893
JohnnyHao <johnny173862903@gmail.com>
2022-06-11 20:41:32 +0800

    2022-06-11

---
+++ [926]将字符串翻转到单调递增.java
@@ -0,0 +1,70 @@
+/**
+<p>如果一个二进制字符串，是以一些 <code>0</code>（可能没有 <code>0</code>）后面跟着一些 <code>1</code>（也可能没有 <code>1</code>）的形式组成的，那么该字符串是 <strong>单调递增 </strong>的。</p>
+
+<p>给你一个二进制字符串 <code>s</code>，你可以将任何 <code>0</code> 翻转为 <code>1</code> 或者将 <code>1</code> 翻转为 <code>0</code> 。</p>
+
+<p>返回使 <code>s</code> 单调递增的最小翻转次数。</p>
+
+<p>&nbsp;</p>
+
+<p><strong>示例 1：</strong></p>
+
+<pre>
+<strong>输入：</strong>s = "00110"
+<strong>输出：</strong>1
+<strong>解释：</strong>翻转最后一位得到 00111.
+</pre>
+
+<p><strong>示例 2：</strong></p>
+
+<pre>
+<strong>输入：</strong>s = "010110"
+<strong>输出：</strong>2
+<strong>解释：</strong>翻转得到 011111，或者是 000111。
+</pre>
+
+<p><strong>示例 3：</strong></p>
+
+<pre>
+<strong>输入：</strong>s = "00011000"
+<strong>输出：</strong>2
+<strong>解释：</strong>翻转得到 00000000。
+</pre>
+
+<p>&nbsp;</p>
+
+<p><strong>提示：</strong></p>
+
+<ul>
+	<li><code>1 &lt;= s.length &lt;= 10<sup>5</sup></code></li>
+	<li><code>s[i]</code> 为 <code>'0'</code> 或 <code>'1'</code></li>
+</ul>
+<div><div>Related Topics</div><div><li>字符串</li><li>动态规划</li></div></div><br><div><li>👍 244</li><li>👎 0</li></div>
+*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minFlipsMonoIncr(String s) {
+        int n = s.length();
+        int dp0 = 0, dp1 = 0;
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            int dp0New = dp0, dp1New = Math.min(dp0, dp1);
+            if (c == '1') {
+                dp0New++;
+            } else {
+                dp1New++;
+            }
+            dp0 = dp0New;
+            dp1 = dp1New;
+        }
+        return Math.min(dp0, dp1);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+class Test {
+    public static void main(String[] args) {
+        System.out.println(new Solution().minFlipsMonoIncr("00011000"));
+    }
+}
