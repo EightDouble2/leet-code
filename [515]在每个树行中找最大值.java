f435be3b10d628094ec940696f033f798dcd8ece
JohnnyHao <johnny173862903@gmail.com>
2022-06-24 22:01:50 +0800

    2022-06-24

---
+++ [515]在每个树行中找最大值.java
@@ -0,0 +1,85 @@
+import javax.swing.tree.TreeNode;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+<p>给定一棵二叉树的根节点&nbsp;<code>root</code> ，请找出该二叉树中每一层的最大值。</p>
+
+<p>&nbsp;</p>
+
+<p><strong>示例1：</strong></p>
+
+<p><img alt="" src="https://assets.leetcode.com/uploads/2020/08/21/largest_e1.jpg" style="height: 172px; width: 300px;" /></p>
+
+<pre>
+<strong>输入: </strong>root = [1,3,2,5,3,null,9]
+<strong>输出: </strong>[1,3,9]
+</pre>
+
+<p><strong>示例2：</strong></p>
+
+<pre>
+<strong>输入: </strong>root = [1,2,3]
+<strong>输出: </strong>[1,3]
+</pre>
+
+<p>&nbsp;</p>
+
+<p><strong>提示：</strong></p>
+
+<ul>
+	<li>二叉树的节点个数的范围是 <code>[0,10<sup>4</sup>]</code></li>
+	<li><meta charset="UTF-8" /><code>-2<sup>31</sup>&nbsp;&lt;= Node.val &lt;= 2<sup>31</sup>&nbsp;- 1</code></li>
+</ul>
+
+<p>&nbsp;</p>
+<div><div>Related Topics</div><div><li>树</li><li>深度优先搜索</li><li>广度优先搜索</li><li>二叉树</li></div></div><br><div><li>👍 245</li><li>👎 0</li></div>
+*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<Integer>();
+        }
+        List<Integer> res = new ArrayList<Integer>();
+        Queue<TreeNode> queue = new ArrayDeque<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            int maxVal = Integer.MIN_VALUE;
+            while (len > 0) {
+                len--;
+                TreeNode t = queue.poll();
+                maxVal = Math.max(maxVal, t.val);
+                if (t.left != null) {
+                    queue.offer(t.left);
+                }
+                if (t.right != null) {
+                    queue.offer(t.right);
+                }
+            }
+            res.add(maxVal);
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
