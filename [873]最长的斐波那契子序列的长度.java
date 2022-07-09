5a0f44efa8df3d16edaa4d8415e837faada44337
JohnnyHao <johnny173862903@gmail.com>
2022-07-09 20:51:57 +0800

    2022-07-09

---
+++ [873]最长的斐波那契子序列的长度.java
@@ -0,0 +1,72 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+<p>如果序列 <code>X_1, X_2, ..., X_n</code> 满足下列条件，就说它是 <em>斐波那契式 </em>的：</p>
+
+<ul>
+	<li><code>n >= 3</code></li>
+	<li>对于所有 <code>i + 2 <= n</code>，都有 <code>X_i + X_{i+1} = X_{i+2}</code></li>
+</ul>
+
+<p>给定一个<strong>严格递增</strong>的正整数数组形成序列 arr ，找到 <font color="#c7254e"><font face="Menlo, Monaco, Consolas, Courier New, monospace"><span style="font-size:12.600000381469727px"><span style="caret-color:#c7254e"><span style="background-color:#f9f2f4">arr</span></span></span></font></font> 中最长的斐波那契式的子序列的长度。如果一个不存在，返回  0 。</p>
+
+<p><em>（回想一下，子序列是从原序列 <font color="#c7254e"><font face="Menlo, Monaco, Consolas, Courier New, monospace"><span style="font-size:12.600000381469727px"><span style="caret-color:#c7254e"><span style="background-color:#f9f2f4">arr</span></span></span></font></font> 中派生出来的，它从 <font color="#c7254e"><font face="Menlo, Monaco, Consolas, Courier New, monospace"><span style="font-size:12.600000381469727px"><span style="caret-color:#c7254e"><span style="background-color:#f9f2f4">arr</span></span></span></font></font> 中删掉任意数量的元素（也可以不删），而不改变其余元素的顺序。例如， <code>[3, 5, 8]</code> 是 <code>[3, 4, 5, 6, 7, 8]</code> 的一个子序列）</em></p>
+
+<p> </p>
+
+<ul>
+</ul>
+
+<p><strong>示例 1：</strong></p>
+
+<pre>
+<strong>输入: </strong>arr =<strong> </strong>[1,2,3,4,5,6,7,8]
+<strong>输出: </strong>5
+<strong>解释: </strong>最长的斐波那契式子序列为 [1,2,3,5,8] 。
+</pre>
+
+<p><strong>示例 2：</strong></p>
+
+<pre>
+<strong>输入: </strong>arr =<strong> </strong>[1,3,7,11,12,14,18]
+<strong>输出: </strong>3
+<strong>解释</strong>: 最长的斐波那契式子序列有 [1,11,12]、[3,11,14] 以及 [7,11,18] 。
+</pre>
+
+<p> </p>
+
+<p><strong>提示：</strong></p>
+
+<ul>
+	<li><code>3 <= arr.length <= 1000</code></li>
+	<li>
+	<p><code>1 <= arr[i] < arr[i + 1] <= 10^9</code></p>
+	</li>
+</ul>
+<div><div>Related Topics</div><div><li>数组</li><li>哈希表</li><li>动态规划</li></div></div><br><div><li>👍 313</li><li>👎 0</li></div>
+*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lenLongestFibSubseq(int[] arr) {
+		Map<Integer, Integer> indices = new HashMap<Integer, Integer>();
+		int n = arr.length;
+		for (int i = 0; i < n; i++) {
+			indices.put(arr[i], i);
+		}
+		int[][] dp = new int[n][n];
+		int ans = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = i - 1; j >= 0 && arr[j] * 2 > arr[i]; j--) {
+				int k = indices.getOrDefault(arr[i] - arr[j], -1);
+				if (k >= 0) {
+					dp[j][i] = Math.max(dp[k][j] + 1, 3);
+				}
+				ans = Math.max(ans, dp[j][i]);
+			}
+		}
+		return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
