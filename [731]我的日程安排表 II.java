1ad820703938b214c9eda63fafcd229c6a0d8389
JohnnyHao <johnny173862903@gmail.com>
2022-07-19 10:08:40 +0800

    2022-07-19

---
+++ [731]我的日程安排表 II.java
@@ -0,0 +1,87 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+<p>实现一个 <code>MyCalendar</code> 类来存放你的日程安排。如果要添加的时间内不会导致三重预订时，则可以存储这个新的日程安排。</p>
+
+<p><code>MyCalendar</code> 有一个 <code>book(int start, int end)</code>方法。它意味着在 <code>start</code> 到 <code>end</code> 时间内增加一个日程安排，注意，这里的时间是半开区间，即 <code>[start, end)</code>, 实数&nbsp;<code>x</code> 的范围为， &nbsp;<code>start &lt;= x &lt; end</code>。</p>
+
+<p>当三个日程安排有一些时间上的交叉时（例如三个日程安排都在同一时间内），就会产生三重预订。</p>
+
+<p>每次调用 <code>MyCalendar.book</code>方法时，如果可以将日程安排成功添加到日历中而不会导致三重预订，返回 <code>true</code>。否则，返回 <code>false</code> 并且不要将该日程安排添加到日历中。</p>
+
+<p>请按照以下步骤调用<code>MyCalendar</code> 类: <code>MyCalendar cal = new MyCalendar();</code> <code>MyCalendar.book(start, end)</code></p>
+
+<p>&nbsp;</p>
+
+<p><strong>示例：</strong></p>
+
+<pre>MyCalendar();
+MyCalendar.book(10, 20); // returns true
+MyCalendar.book(50, 60); // returns true
+MyCalendar.book(10, 40); // returns true
+MyCalendar.book(5, 15); // returns false
+MyCalendar.book(5, 10); // returns true
+MyCalendar.book(25, 55); // returns true
+<strong>解释：</strong> 
+前两个日程安排可以添加至日历中。 第三个日程安排会导致双重预订，但可以添加至日历中。
+第四个日程安排活动（5,15）不能添加至日历中，因为它会导致三重预订。
+第五个日程安排（5,10）可以添加至日历中，因为它未使用已经双重预订的时间10。
+第六个日程安排（25,55）可以添加至日历中，因为时间 [25,40] 将和第三个日程安排双重预订；
+时间 [40,50] 将单独预订，时间 [50,55）将和第二个日程安排双重预订。
+</pre>
+
+<p>&nbsp;</p>
+
+<p><strong>提示：</strong></p>
+
+<ul>
+	<li>每个测试用例，调用&nbsp;<code>MyCalendar.book</code>&nbsp;函数最多不超过&nbsp;<code>1000</code>次。</li>
+	<li>调用函数&nbsp;<code>MyCalendar.book(start, end)</code>时，&nbsp;<code>start</code> 和&nbsp;<code>end</code> 的取值范围为&nbsp;<code>[0, 10^9]</code>。</li>
+</ul>
+<div><div>Related Topics</div><div><li>设计</li><li>线段树</li><li>二分查找</li><li>有序集合</li></div></div><br><div><li>👍 140</li><li>👎 0</li></div>
+*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class MyCalendarTwo {
+    Map<Integer, int[]> tree;
+
+    public MyCalendarTwo() {
+        tree = new HashMap<Integer, int[]>();
+    }
+
+    public boolean book(int start, int end) {
+        update(start, end - 1, 1, 0, 1000000000, 1);
+        tree.putIfAbsent(1, new int[2]);
+        if (tree.get(1)[0] > 2) {
+            update(start, end - 1, -1, 0, 1000000000, 1);
+            return false;
+        }
+        return true;
+    }
+
+    public void update(int start, int end, int val, int l, int r, int idx) {
+        if (r < start || end < l) {
+            return;
+        }
+        tree.putIfAbsent(idx, new int[2]);
+        if (start <= l && r <= end) {
+            tree.get(idx)[0] += val;
+            tree.get(idx)[1] += val;
+        } else {
+            int mid = (l + r) >> 1;
+            update(start, end, val, l, mid, 2 * idx);
+            update(start, end, val, mid + 1, r, 2 * idx + 1);
+            tree.putIfAbsent(2 * idx, new int[2]);
+            tree.putIfAbsent(2 * idx + 1, new int[2]);
+            tree.get(idx)[0] = tree.get(idx)[1] + Math.max(tree.get(2 * idx)[0], tree.get(2 * idx + 1)[0]);
+        }
+    }
+}
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo obj = new MyCalendarTwo();
+ * boolean param_1 = obj.book(start,end);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
